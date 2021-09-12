a056f83ab4f750fa78f2748dda74f4716f678034
Artiom <Artiom@DESKTOP-RGF1328>
2021-09-12 20:55:35 +0300

    Added implementation for insertCard() method

--- src/main/java/atm/CardController.java
+++ src/main/java/atm/CardController.java
@@ -7,25 +7,61 @@ import java.util.Random;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.FileChooser;
 
+/**
+ * CardController to create/insert cards and perform different actions
+ * @author Artiom
+ *
+ */
 public class CardController {
 
+	// Button object just to get the scene/window or other
 	@FXML
 	private Button buttonCreateCard;
 
+	// Stores currentCard(current .atm file)
+	private File currentCard;
+
 	/**
-	 * Gets the inserted card(.txt file)
+	 * Gets the inserted card(.atm file)
 	 * 
 	 * @param event - Event gotten from Scene
 	 */
 	public void insertCard(ActionEvent event) {
+		try {
+			// Creates FileChooser to select file to open
+			FileChooser fileChooser = new FileChooser();
+			// Extension filter to show only .atm files
+			fileChooser.getExtensionFilters()
+					.addAll(new FileChooser.ExtensionFilter("ATM Machine Files (.atm)", "*.atm"));
 
+			// Stores the selected file
+			File selectedCard = fileChooser.showOpenDialog(buttonCreateCard.getScene().getWindow());
+
+			// If selected file doesn't end with .atm
+			if (!selectedCard.toString().toLowerCase().endsWith(".atm")) {
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setTitle("Error");
+				alert.setHeaderText(null);
+				alert.setContentText("This is not a .atm file! Create a card if you don't have one!");
+				alert.showAndWait();
+				return;
+			}
+
+			// Sets current card to opened card
+			currentCard = selectedCard;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
-	 * Creates a new card(.txt file)
+	 * Creates a new card(.atm file)
 	 * 
 	 * @param event - Event gotten from Scene
 	 */
@@ -33,15 +69,16 @@ public class CardController {
 		try {
 			// Creates FileChooser to select save file
 			FileChooser fileChooser = new FileChooser();
-			// Extension filter to show only .txt files
-			fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+			// Extension filter to show only .atm files
+			fileChooser.getExtensionFilters()
+					.addAll(new FileChooser.ExtensionFilter("ATM Machine Files (.atm)", "*.atm"));
 
 			// Stores the selected file
 			File newCard = fileChooser.showSaveDialog(buttonCreateCard.getScene().getWindow());
 
 			// If selected file doesn't have extension add it manually
-			if (!newCard.toString().toLowerCase().endsWith(".txt")) {
-				newCard = new File(newCard.getAbsolutePath() + ".txt");
+			if (!newCard.toString().toLowerCase().endsWith(".atm")) {
+				newCard = new File(newCard.getAbsolutePath() + ".atm");
 			}
 
 			// Creates BufferedWriter to write info about the card
@@ -54,6 +91,9 @@ public class CardController {
 
 			// Closes the opened file
 			writer.close();
+
+			// Sets current card to new created card
+			currentCard = newCard;
 
 		} catch (Exception e) {
 			e.printStackTrace();
