62a8b51c3f6f53018ac3e9bb85ebf2c276c54713
Artiom <Artiom@DESKTOP-RGF1328>
2021-09-15 18:00:15 +0300

    Changed names of methods

--- src/main/java/atm/SceneController.java
+++ src/main/java/atm/SceneController.java
@@ -20,14 +20,14 @@ public class SceneController {
 	private Parent root;
 
 	/**
-	 * Changes the scene to Scene2
+	 * Switches to card management scene
 	 * 
 	 * @param event - Event gotten from Scene
 	 */
-	public void changeToScene2(ActionEvent event) {
+	public void switchToManagementScene(ActionEvent event) {
 		try {
-			// Gets Scene2 and the stage
-			root = FXMLLoader.load(getClass().getResource("/Scene2.fxml"));
+			// Gets managementScene and the stage
+			root = FXMLLoader.load(getClass().getResource("/managementScene.fxml"));
 			stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 			scene = new Scene(root);
 			stage.setScene(scene);
@@ -43,14 +43,37 @@ public class SceneController {
 	}
 
 	/**
-	 * Changes the scene to Scene1
+	 * Switches to insert card scene
 	 * 
 	 * @param event - Event gotten from Scene
 	 */
-	public void changeToScene1(ActionEvent event) {
+	public void switchToInsertScene(ActionEvent event) {
 		try {
-			// Gets Scene1 and the stage
-			root = FXMLLoader.load(getClass().getResource("/Scene1.fxml"));
+			// Gets insertScene and the stage
+			root = FXMLLoader.load(getClass().getResource("/insertScene.fxml"));
+			stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+
+			// Sets css styles for the scene
+			scene.getStylesheets().add(this.getClass().getResource("/Styles.css").toExternalForm());
+
+			stage.show();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Switches to pin scene
+	 * 
+	 * @param event - Event gotten from Scene
+	 */
+	public void switchToPinScene(ActionEvent event) {
+		try {
+			// Gets pinScene and the stage
+			root = FXMLLoader.load(getClass().getResource("/pinScene.fxml"));
 			stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 			scene = new Scene(root);
 			stage.setScene(scene);
