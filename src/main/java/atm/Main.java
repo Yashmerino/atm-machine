594b57965e4af1a5ddf0559b22f95fde9b9f4564
Artiom <Artiom@DESKTOP-RGF1328>
2021-09-17 10:39:50 +0300

    Added additional main class for exporting

---
+++ src/main/java/atm/Main.java
@@ -0,0 +1,15 @@
+package atm;
+
+/**
+ * Main class to run the application
+ * 
+ * @author Artiom
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		App.main(args);
+	}
+
+}
